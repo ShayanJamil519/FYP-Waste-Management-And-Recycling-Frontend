0235c49a63ee037bda33280b4c50046ffa4ce890
ShayanJamil519 <shayanjamil500@gmail.com>
2023-12-03 12:35:30 +0500

    How It Works Section In Progress

--- src/app/data.js
+++ src/app/data.js
@@ -18,3 +18,27 @@ export const heroSliderImages = [
     imageSrc: "/home/hero__slider1.jpg",
   },
 ];
+
+export const howItWorksHomeData = [
+  {
+    imageSrc: "/home/how__it__works2.png",
+    title: " Great Service & Low Cost",
+  },
+  {
+    imageSrc: "/home/how__it__works3.png",
+    title: "Carbon Trust Certified",
+  },
+  {
+    imageSrc: "/home/how__it__works4.png",
+    title: "Trusted & Local Experts",
+  },
+];
+
+export const howItWorksHomeListData = [
+  {
+    list: "Commercial & Business Waste Management Experts",
+  },
+  {
+    list: "Provide Regular, Reliable and Convenient Services",
+  },
+];
--- src/components/Home/HowItWorks.jsx
+++ src/components/Home/HowItWorks.jsx
@@ -1,5 +1,9 @@
+"use client";
+import { howItWorksHomeData, howItWorksHomeListData } from "@/app/data";
 import React from "react";
+import { FaPlay } from "react-icons/fa";
 import { IoIosArrowRoundForward } from "react-icons/io";
+import { IoIosCheckmark } from "react-icons/io";
 
 const HowItWorks = () => {
   return (
@@ -38,6 +42,121 @@ const HowItWorks = () => {
           <div className="p-3 w-fit absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#257830] transition duration-500  text-[#fff] hover:scale-110 cursor-pointer shadow-bubble">
             <IoIosArrowRoundForward className="text-[28px] font-bold" />
           </div>{" "}
+        </div>
+      </div>
+      {/* Bottom Container */}
+      <div className="flex justify-between items-start gap-10 mt-20">
+        {/* Left Container */}
+        <div
+          style={{
+            background:
+              "url('/home/how__it__works1.jpg') no-repeat center center/cover",
+          }}
+          className="w-[60%] h-[550px] rounded-3xl relative"
+        >
+          <div className="absolute -left-12 bottom-28  w-full  h-48 flex justify-center items-stretch">
+            <div className="w-[15%] bg-[#f29620] grid place-items-center rounded-s-xl cursor-pointer transition duration-500 ease-in-out hover:bg-[#257830]">
+              <FaPlay className="text-[20px] text-[#fff]" />
+            </div>
+            <div className="w-full flex justify-center items-center rounded-e-xl py-10 bg-[#fff]">
+              {howItWorksHomeData.map((item) => (
+                <div
+                  key={item.title}
+                  className="px-7 flex flex-col justify-center items-center border-r-gray-300 border-r-2"
+                >
+                  <img
+                    src={item.imageSrc}
+                    alt="logo"
+                    className="transition duration-400 ease-in-out hover:scale-110"
+                  />
+                  <h1 className="mt-3 text-center text-[#182822] font-semibold font-poppins">
+                    {item.title}
+                  </h1>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        {/* Right Container */}
+        <div className="w-[40%] font-poppins ">
+          <p className="mb-5 text-[#63716c]">
+            We have already made huge strides in our sustainability journey by
+            investing in plastic recycling and energy from waste infrastructure
+            and low carbon collections, leading to a huge reduction in nation’s
+            carbon smoke.
+          </p>
+          <p className=" text-[#63716c]">
+            We already made huge strides in our sustainability journey by
+            investing in recycling, offering low cost commercial waste
+            collection services to meet all your waste.
+          </p>
+          <div className="w-full flex justify-center items-start rounded-md my-7 gap-5 py-7 px-7 bg-[#f7f9f8]">
+            <img
+              src="/home/how__it__works5.png"
+              alt="logo"
+              className="transition duration-400 ease-in-out hover:scale-110"
+            />
+            <div className="font-poppins">
+              <p className="font-semibold text-[#58635f]">
+                We will help you manage all waste removal, regardless of the
+                size of waste, we will treat your trash in the best way for
+                environment.
+              </p>
+              <div className="hover_effect flex justify-start items-center gap-3 mt-2 cursor-pointer ">
+                <span className="text-[#257830] text-[15px] font-semibold font-poppins">
+                  See How It Works?
+                </span>
+                <span className="p-1 rounded-full bg-[#257830] transition duration-500  text-[#fff] ">
+                  <IoIosArrowRoundForward className="text-[25px] font-bold" />
+                </span>{" "}
+                <style jsx>{`
+                  .hover_effect:hover span:nth-child(2) {
+                    transform: scale(1.2);
+                  }
+                `}</style>
+              </div>
+            </div>
+          </div>
+          <p className=" text-[#63716c]">
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour
+          </p>
+          {/* Buttons Container */}
+          <div className="font-poppins my-7 flex justify-start items-center gap-7 text-[15px] font-semibold">
+            <button className="flex justify-center items-center gap-3 bg-[#20332c] transition duration-500 ease-in-out hover:bg-[#257830] text-[#fff] outline-none border-0 px-10 py-5 rounded-sm">
+              Explore Products{" "}
+              <span className="p-0 rounded-full bg-[#fff]  transition duration-500 text-[#20332c] ">
+                <IoIosArrowRoundForward className="text-[27px] font-bold" />
+              </span>{" "}
+              <style jsx>{`
+                button:hover span {
+                  background-color: #fff;
+                  color: #257830;
+                }
+              `}</style>
+            </button>
+            {/* ==== */}
+            <button className="flex justify-center items-center gap-3 bg-[#fff] text-[#257830] border-2 border-[#257830] transition duration-500 ease-in-out hover:bg-[#257830] hover:text-[#fff] outline-none  px-10 py-[22px] rounded-sm">
+              About Us
+            </button>
+          </div>
+          {/* List */}
+          <div className="w-full">
+            {howItWorksHomeListData.map((item) => (
+              <div
+                key={item.list}
+                className="flex justify-start items-center gap-5 py-5 border-b-[1px] border-[#d9e3e0]"
+              >
+                <div className="w-fit p-1 bg-[#f7f9f8]  rounded-full">
+                  <IoIosCheckmark className="text-[#257830] text-[20px]" />
+                </div>
+                <p className="font-semibold text-[#58635f] text-sm">
+                  {item.list}
+                </p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
