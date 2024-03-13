9ec89ed2a557674e979f0e61dadf85838586e41c
ShayanJamil519 <shayanjamil500@gmail.com>
2024-03-13 20:36:20 +0500

    Deployment Fixes

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -8,7 +8,7 @@ export default function NotFound() {
           <h1 className="font-bold text-blue-600 text-9xl">404</h1>
           <h2 className="text-3xl font-semibold">Page Not Found</h2>
           <p className="mt-4 text-gray-500 font-poppins">
-            Sorry, we couldn't find the page you're looking for.
+            {`Sorry, we couldn't find the page you're looking for.`}
           </p>
           <Link
             href="/"
