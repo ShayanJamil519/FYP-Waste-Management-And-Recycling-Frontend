4b62d6f1e2b3c3beb206d72d7a3aa73ce4d3e177
Raza <mraza6601@gmail.com>
2024-03-28 10:31:22 +0500

    create thread done

--- src/app/user/home/page.jsx
+++ src/app/user/home/page.jsx
@@ -1,16 +1,55 @@
 "use client";
 import React, { useState } from "react";
 import '../../../index.css';
+import { useCreateThread } from "../../../hooks/thread-hook";
+import { useStateContext } from "@/app/StateContext";
+import { toast } from "react-toastify";
+import { usePathname, useRouter } from "next/navigation";
+import { FaSpinner } from "react-icons/fa";
 
 
 const Page = () => {
     const [thread, setThread] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const { user, setUser } = useStateContext();
+    const [userData, setUserData] = useState({
+        userId: user?._id,
+        title: "",
+      });
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log({ thread });
-        setThread("");
-    };
+      const { mutate: addMutate } = useCreateThread(JSON.stringify(userData));
+
+      const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setUserData({
+          ...userData,
+          [name]: value,
+        });
+        console.log(userData.title)
+      };
+    
+      const handleSubmit = async (event) => {
+        event.preventDefault();
+        setIsLoading(true);
+        addMutate(
+          {},
+          {
+            onSuccess: (response) => {
+              toast.success(response?.data?.message);
+              
+              setIsLoading(false);
+            },
+            onError: (response) => {
+              console.error("An error occurred:");
+              console.log(response);
+              console.log(response.response.data.message);
+              toast.error(response.response.data.message);
+              setIsLoading(false);
+            },
+          }
+        );
+      };
+    
     return (
         <>
             <main className='home'>
@@ -20,13 +59,17 @@ const Page = () => {
                         <label htmlFor='thread'>Title / Description</label>
                         <input
                             type='text'
-                            name='thread'
+                            name='title'
                             required
-                            value={thread}
-                            onChange={(e) => setThread(e.target.value)}
+                            onChange={handleInputChange}
                         />
                     </div>
+                    {isLoading ? (
+                  <FaSpinner className="animate-spin" /> // Show spinner if isLoading is true
+                ) : (
                     <button className='homeBtn'>CREATE THREAD</button>
+                )}
+                    
                 </form>
             </main>
         </>
