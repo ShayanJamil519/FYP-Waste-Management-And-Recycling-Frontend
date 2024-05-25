34ca0129c14829c906f0460bc692a02427f7ef14
ShayanJamil519 <shayanjamil500@gmail.com>
2024-05-25 06:57:56 +0500

    Email Js Integrated In Contact Page

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -1,21 +1,65 @@
 "use client";
 import React, { useState } from "react";
 import Input from "../CC/Input";
-import Select from "../CC/Select";
+import emailjs from "@emailjs/browser";
 import TextArea from "../CC/TextArea";
 import { IoIosArrowRoundForward } from "react-icons/io";
+import { toast } from "react-toastify";
 
 const ContactForm = () => {
-  const [selectedOption, setSelectedOption] = useState(null);
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
 
-  const [textValue, setTextValue] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const handleTextChange = (text) => {
-    setTextValue(text);
+  const handleChange = (e) => {
+    const { target } = e;
+    const { name, value } = target;
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
   };
 
-  const handleSelectChange = (option) => {
-    setSelectedOption(option);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        process.env.NEXT_PUBLIC_SERVICE_ID,
+        process.env.NEXT_PUBLIC_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: process.env.NEXT_PUBLIC_EMAIL_NAME,
+          from_email: form.email,
+          to_email: process.env.NEXT_PUBLIC_EMAIL_TO,
+          message: form.message,
+        },
+        process.env.NEXT_PUBLIC_PUBLIC_KEY
+      )
+      .then(
+        () => {
+          setLoading(false);
+          toast.success(
+            "Thanks for contacting. We will get back to you as soon as possible."
+          );
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          toast.error("Looks like something went wrong. Please try again.");
+        }
+      );
   };
 
   return (
@@ -31,30 +75,40 @@ const ContactForm = () => {
       <p className="text-sm mt-3 leading-6 text-[#ffa500]">
         Contact Us And We Will Respond Within The Next Two Working Days.
       </p>
-      <form className="w-full mt-10 ">
+      <form className="w-full mt-10 " onSubmit={handleSubmit}>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <Input
+            onChange={handleChange}
             label="Name"
             type="text"
             placeholder="Please write your name"
+            require={true}
+            name="name"
           />
 
           <Input
+            onChange={handleChange}
             label="Email"
             type="email"
             placeholder="Please write your email"
+            require={true}
+            name="email"
           />
         </div>
         <TextArea
-          value={textValue}
-          onChange={handleTextChange}
+          onChange={handleChange}
           placeholder="Enter your text here..."
           rows={6}
           label="Your Query"
+          require={true}
+          name="message"
         />
 
-        <button className="mt-6 w-full flex justify-center items-center font-semibold text-sm gap-3 bg-[#20332c] transition duration-500 ease-in-out hover:bg-[#257830] text-[#fff] hover:text-[#fff] outline-none border-0 px-7 py-5 rounded-md sm:rounded-sm">
-          Submit Request
+        <button
+          type="submit"
+          className="mt-6 w-full flex justify-center items-center font-semibold text-sm gap-3 bg-[#20332c] transition duration-500 ease-in-out hover:bg-[#257830] text-[#fff] hover:text-[#fff] outline-none border-0 px-7 py-5 rounded-md sm:rounded-sm"
+        >
+          {loading ? "Submitting ..." : "Submit Request"}
           <span className="p-0 rounded-full bg-[#fff]  transition duration-500 text-[#20332c] ">
             <IoIosArrowRoundForward className="text-[27px] font-bold" />
           </span>{" "}
