a461d0dffe130abaf31de227ac2a0e6b2a07f730
ShayanJamil519 <shayanjamil500@gmail.com>
2024-07-09 18:57:16 +0500

    Edit Complaint Modal Fixes

--- src/components/Dashboard/DistrictAdmin/EditComplainModal.jsx
+++ src/components/Dashboard/DistrictAdmin/EditComplainModal.jsx
@@ -11,7 +11,7 @@ import { RxCross1 } from "react-icons/rx";
 import WrappedMapComponent from "./Map";
 import { FaSpinner } from "react-icons/fa";
 import { useStateContext } from "@/app/StateContext";
-import {  useQueryClient } from "@tanstack/react-query";
+import { useQueryClient } from "@tanstack/react-query";
 
 const Images = [
   "/home/hero__slider1.jpg",
@@ -22,16 +22,20 @@ const Images = [
   "/home/hero__slider1.jpg",
 ];
 
-
-const EditComplainModal = ({ setOpenEditComplainModal, complaintId , latitude , longitude , imagee}) => {
-  console.log(imagee)
+const EditComplainModal = ({
+  setOpenEditComplainModal,
+  complaintId,
+  latitude,
+  longitude,
+  imagee,
+}) => {
+  console.log(imagee);
   const { user } = useStateContext();
   const router = useRouter();
   const queryClient = useQueryClient();
   const [isLooading, setIsLoading] = useState(false);
   const [image, setImage] = useState(null);
   const [info, setInfo] = useState({
-
     comments: "",
     image: "",
   });
@@ -68,10 +72,13 @@ const EditComplainModal = ({ setOpenEditComplainModal, complaintId , latitude , 
     console.log("complaintttttttttID");
     console.log(complaintId);
     setIsLoading(true);
-    console.log(info)
+    console.log(info);
     try {
       await addResponse(complaintId, info);
-      await queryClient.invalidateQueries(['complaint/get-complaints-district', user?.district]);
+      await queryClient.invalidateQueries([
+        "complaint/get-complaints-district",
+        user?.district,
+      ]);
       setIsLoading(false);
       setOpenEditComplainModal(false);
       // Optionally, you can add a redirect or any other logic here after successful response
@@ -94,21 +101,31 @@ const EditComplainModal = ({ setOpenEditComplainModal, complaintId , latitude , 
 
   return (
     <Modal onClose={() => setOpenEditComplainModal(false)}>
-      <div className="h-[90vh] overflow-y-auto p-4 sm:p-5 md:p-10 bg-[#fff] rounded-md border-2 border-[#000] font-urbanist w-[97%] sm:w-[65%] md:w-[55%] lg:w-[80%] mx-auto">
-        <h1 className="font-bold text-3xl">Add Response to a complain</h1>
+      <div className="h-[90vh] overflow-y-auto p-4 sm:p-5 md:p-10 bg-[#fff] rounded-md border-2 border-[#000] font-poppins w-[97%] sm:w-[65%] md:w-[55%] lg:w-[80%] mx-auto">
+        <h1 className="font-semibold text-3xl">Add Response to a complain</h1>
         <p className="text-sm mt-3 leading-6 text-[#62706b]">
           Please complete the form below, to update a user about response on
           their complain
         </p>
-        <form className="w-full mt-10" onSubmit={handleAddResponse}>
+
+        <div className="w-full mt-10  h-[250px] rounded-md  text-gray-700">
+          <h1 className="font-semibold text-xl">Latest waste of site</h1>
+          <img
+            src={imagee}
+            alt="Location"
+            className="w-full h-full rounded-md"
+          />
+        </div>
+
+        <form className="w-full mt-16" onSubmit={handleAddResponse}>
           <div className="my-3">
             {image ? (
               <div className="">
-                <div className="w-24 h-24 mx-auto relative">
+                <div className="w-[150px] h-24 mx-auto relative">
                   <img
                     src={image}
                     alt="Avatar"
-                    className="rounded-full w-full h-full  "
+                    className="rounded-md w-full h-full  "
                   />
                   <button
                     onClick={removeAvatar}
@@ -140,8 +157,6 @@ const EditComplainModal = ({ setOpenEditComplainModal, complaintId , latitude , 
             />
           </div>
 
-          
-
           <TextArea
             name="comments"
             value={info.comments}
@@ -150,34 +165,38 @@ const EditComplainModal = ({ setOpenEditComplainModal, complaintId , latitude , 
             rows={6}
             label="Please write your details"
           />
-          
-        <div className="w-full h-32 bg-gray-200 rounded-md flex flex-col items-center justify-center text-gray-700">
-          <img src={imagee} alt="Location" className="w-full h-full object-cover rounded-md" />
-        </div>
-      
+
           <WrappedMapComponent latitude={latitude} longitude={longitude} />
 
-<div className="grid place-items-center mt-6">
-          {isLooading ? (
-            <FaSpinner className="animate-spin" /> // Show spinner if isLoading is true
-          ) : (
-            <button
-              type="submit"
-              className="mt-6 w-full flex justify-center items-center font-semibold text-sm gap-3 bg-[#20332c] transition duration-500 ease-in-out hover:bg-[#257830] text-[#fff] hover:text-[#fff] outline-none border-0 px-7 py-5 rounded-sm"
-            >
-              Submit
-              <span className="p-0 rounded-full bg-[#fff]  transition duration-500 text-[#20332c] ">
-                <IoIosArrowRoundForward className="text-[27px] font-bold" />
-              </span>{" "}
-              <style jsx>{`
-                button:hover span {
-                  background-color: #fff;
-                  color: #257830;
-                }
-              `}</style>
-            </button>
-          )}
-        </div>
+          <div className="grid place-items-center mt-6">
+            {isLoading ? (
+              <button
+                type="submit"
+                className=" w-full flex justify-center items-center font-semibold text-sm sm:text-lg gap-3 bg-[#20332c] transition duration-500 ease-in-out outline-none border-0 px-7 lg:py-4 py-3 rounded-md"
+                disabled
+              >
+                <FaSpinner className="animate-spin mr-2 text-white" />
+                <span className={"text-white"}>Loading...</span>
+              </button>
+            ) : (
+              <button
+                type="submit"
+                // onClick={resetForm}
+                className=" w-full flex justify-center items-center font-semibold text-sm sm:text-lg gap-3 bg-[#20332c] transition duration-500 ease-in-out hover:bg-[#257830] text-[#fff] hover:text-[#fff] outline-none border-0 px-7 lg:py-4 py-3 rounded-md"
+              >
+                Submit
+                <span className="p-0 rounded-full bg-[#fff] transition duration-500 text-[#20332c]">
+                  <IoIosArrowRoundForward className="text-[27px] font-bold" />
+                </span>{" "}
+                <style jsx>{`
+                  button:hover span {
+                    background-color: #fff;
+                    color: #257830;
+                  }
+                `}</style>
+              </button>
+            )}
+          </div>
         </form>
       </div>
     </Modal>
