b3bc4536f2e137ca74ad0139b7072650c747f8a5
ShayanJamil519 <shayanjamil500@gmail.com>
2024-07-10 14:00:33 +0500

    All Users Table Fixes

--- src/components/Dashboard/Admin/AllUsers.jsx
+++ src/components/Dashboard/Admin/AllUsers.jsx
@@ -25,23 +25,23 @@ const AllUsers = () => {
   const paginate = usePagination();
 
   const { data, isLoading, isError } = useGetAllUsers();
-    console.log(data)
-    // Check loading and error states
-    if (isLoading) {
-      return (
-        <div className="w-full h-[70vh] flex justify-center items-center">
-          <DataLoader />
-        </div>
-      );
-    }
-  
-    if (isError) {
-      return <div>Error loading complaints</div>;
-    }
-  
-    const { currentPage, totalPages, visibleItems, goToPage } = paginate(
-      data && data?.users
+  console.log(data);
+  // Check loading and error states
+  if (isLoading) {
+    return (
+      <div className="w-full h-[70vh] flex justify-center items-center">
+        <DataLoader />
+      </div>
     );
+  }
+
+  if (isError) {
+    return <div>Error loading complaints</div>;
+  }
+
+  const { currentPage, totalPages, visibleItems, goToPage } = paginate(
+    data && data?.users
+  );
 
   return (
     <div>
@@ -49,26 +49,24 @@ const AllUsers = () => {
       <div className="rounded-sm border border-stroke bg-white shadow-default  font-poppins ">
         <div className="py-4 px-4 md:px-6 xl:px-7.5">
           <h4 className="text-xl font-semibold text-black dark:text-white">
-            Top Products
+            All Users
           </h4>
         </div>
 
-        <div className="grid grid-cols-7 border-t border-stroke py-4 px-4  sm:grid-cols-8 md:px-6 2xl:px-7">
-          <div className="col-span-3 flex items-center">
+        <div className="grid grid-cols-7 border-t border-stroke py-4 px-4  sm:grid-cols-4 md:px-6 2xl:px-7">
+          <div className="col-span-1 flex items-center">
             <p className="font-medium">District</p>
           </div>
 
+          <div className="col-span-1 flex items-center">
+            <p className="font-medium">User ID</p>
+          </div>
+
           <div className=" flex items-center">
-            <p className="font-medium">Description</p>
+            <p className="font-medium">Email</p>
           </div>
           <div className=" flex items-center">
-            <p className="font-medium">Response</p>
-          </div>
-          <div className=" flex items-center">
-            <p className="font-medium">Date</p>
-          </div>
-          <div className=" flex items-center">
-            <p className="font-medium">Actions</p>
+            <p className="font-medium">ETH Address</p>
           </div>
         </div>
 
@@ -76,56 +74,51 @@ const AllUsers = () => {
         <div className="h-[55vh] overflow-auto">
           {visibleItems.map((product, key) => (
             <div
-              className=" grid grid-cols-7 border-t border-stroke py-2 px-4  sm:grid-cols-8 md:px-6 2xl:px-7"
+              className=" grid grid-cols-7 border-t border-stroke py-2 px-4  sm:grid-cols-4 md:px-6 2xl:px-7"
               key={key}
             >
-              <div className="col-span-3 flex items-center">
+              <div className="col-span-1 flex items-center">
                 <div className="flex flex-col gap-4 sm:flex-row sm:items-center">
-                  <div className=" w-20 h-20 rounded-md">
-                    <img src={product.avatar.url} alt="Product" />
+                  <div className=" ">
+                    <img
+                      src={product?.avatar?.url}
+                      alt="Product"
+                      className="w-20  rounded-xl"
+                    />
                   </div>
                   <p className="text-sm text-black dark:text-white">
-                    {product.district}
+                    {product?.district}
                   </p>
                 </div>
               </div>
 
               <div className=" flex items-center">
                 <p className="text-sm text-black dark:text-white">
-                  {product.email}
+                  {product?._id}
+                </p>
+              </div>
+
+              <div className=" flex items-center">
+                <p className="text-sm text-black dark:text-white">
+                  {product?.email}
                 </p>
               </div>
               <div className=" flex items-center">
                 <p className="text-sm text-black dark:text-white">
-                  {product.ethAddress
-}
+                  {product?.ethAddress}
                 </p>
-              </div>
-              <div className=" flex items-center">
-                <p className="text-sm text-meta-3">{product.name}</p>
-              </div>
-              <div className=" flex gap-3 justify-start items-center text-[20px]">
-                <MdEdit
-                  className="cursor-pointer"
-                  onClick={() => setOpenEditUserModal(true)}
-                />
-                <MdDelete className="cursor-pointer" />
               </div>
             </div>
           ))}
         </div>
       </div>
       {/* Pagination */}
-      <Pagination
-        totalPages={totalPages}
-        currentPage={currentPage}
-        onPageChange={goToPage}
-      />
-
-      {/* Edit Modal */}
-
-      {openEditUserModal && (
-        <EditUserModal setOpenEditUserModal={setOpenEditUserModal} />
+      {visibleItems?.length > 5 && (
+        <Pagination
+          totalPages={totalPages}
+          currentPage={currentPage}
+          onPageChange={goToPage}
+        />
       )}
     </div>
   );
