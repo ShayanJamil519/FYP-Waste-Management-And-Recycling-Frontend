838629d2cc2c52490c511e46c686c8d2f1ef30f9
ShayanJamil519 <shayanjamil500@gmail.com>
2024-07-11 14:30:25 +0500

    Footer Fixes

--- src/components/Shared/Footer.jsx
+++ src/components/Shared/Footer.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 import { FaPhoneAlt } from "react-icons/fa";
 import { IoIosArrowRoundForward, IoMdMail } from "react-icons/io";
@@ -5,23 +6,25 @@ import { IoIosArrowRoundForward, IoMdMail } from "react-icons/io";
 const Footer = () => {
   return (
     <footer className="bg-[#111d19]  font-poppins text-white text-[15px] py-10 px-4 md:px-16">
-      <div className=" mx-auto grid grid-cols-1 md:grid-cols-6 gap-10 md:gap-16 ">
+      <div className=" mx-auto grid grid-cols-1 md:grid-cols-6 gap-10 md:gap-10 ">
         {/* Fourth Column */}
         <div className="md:col-span-2">
           <p className="mb-4">
             We recognize that the right service for home is most important
-            chosie, Trashco can provide the waste collection you need for your
-            home, with trusted service.
+            choice, Recy-sense can provide the waste collection you need for
+            your home, with trusted service.
           </p>
           <div className="font-bold text-[#f29620]">
             <div className="flex justify-start items-center gap-3 mb-3">
               <IoMdMail className="text-[25px]" />
-              <p>abc.com</p>
+              <a href="mailto:recy8001@gmail.com" className="hover:underline">
+                recy8001@gmail.com
+              </a>
             </div>
 
             <div className="flex justify-start items-center gap-3">
               <FaPhoneAlt className="text-[20px]" />
-              <p className="mt-1">9084084-0-1-41041-</p>
+              <p className="mt-1">+92 333 0987838</p>
             </div>
           </div>
           <div className="flex mt-4"></div>
@@ -32,71 +35,70 @@ const Footer = () => {
           <h5 className="text-xl font-bold mb-2 sm:mb-6">Company</h5>
           <ul>
             <li className="mb-3 sm:mb-4">
-              <a href="#" className="hover:underline">
+              <Link href="/about" className="hover:underline">
                 About Us
-              </a>
+              </Link>
             </li>
             <li className="mb-3 sm:mb-4">
-              <a href="#" className="hover:underline">
-                Sustainability
-              </a>
+              <Link href="/forum" className="hover:underline">
+                Community Forum
+              </Link>
             </li>
             <li className="mb-3 sm:mb-4">
-              <a href="#" className="hover:underline">
-                Leadership Team
-              </a>
+              <Link href="/contact" className="hover:underline">
+                Contact Us
+              </Link>
             </li>
             <li className="mb-3 sm:mb-4">
-              <a href="#" className="hover:underline">
-                Blog Grid
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline">
-                Blog Standard
-              </a>
+              <Link href="/complain" className="hover:underline">
+                Complain
+              </Link>
             </li>
           </ul>
         </div>
 
         {/* Second Column */}
         <div>
-          <h5 className="text-xl font-bold mb-2 sm:mb-6">Services</h5>
+          <h5 className="text-xl font-bold mb-2 sm:mb-6">Entities</h5>
           <ul>
             <li className="mb-3 sm:mb-4">
               <a href="#" className="hover:underline">
-                Residential Waste
+                User
               </a>
             </li>
-            {/* ... Other services */}
+            <li className="mb-3 sm:mb-4">
+              <a href="#" className="hover:underline">
+                District Admin
+              </a>
+            </li>
+            <li className="mb-3 sm:mb-4">
+              <a href="#" className="hover:underline">
+                Recycling Point Admin
+              </a>
+            </li>
+            <li className="mb-3 sm:mb-4">
+              <a href="#" className="hover:underline">
+                Landfill Admin
+              </a>
+            </li>
+            <li className="mb-3 sm:mb-4">
+              <a href="#" className="hover:underline">
+                Super Admin
+              </a>
+            </li>
           </ul>
         </div>
 
         {/* Fourth Column */}
         <div className="md:col-span-2">
-          <h5 className="text-xl font-bold mb-2 sm:mb-6">Newsletter</h5>
+          <h5 className="text-xl font-bold mb-2 sm:mb-6">Recy-sense</h5>
           <p className="mb-4">
             Sign up for industry alerts, deals, news and insights from Trashco
-            company.
+            company.Leveraging blockchain technology, our software ensures
+            transparency and immutability, holding government authorities
+            accountable for efficient waste management and empowering citizens
+            with reliable data.
           </p>
-          <div className="flex  mt-4 bg-[#fff] w-full rounded-md justify-center items-center">
-            <input
-              type="email"
-              className="py-4 px-3 w-[90%] focus:outline-none text-[#20332c] rounded-md"
-              placeholder="Your Email Address"
-            />
-            <button className="p-1 bg-[#20332c] mr-2 w-fit h-fit rounded-full  focus:outline-none">
-              <span className="p-0 rounded-full  text-[#fff]  ">
-                <IoIosArrowRoundForward className="text-[23px] font-bold" />
-              </span>{" "}
-              <style jsx>{`
-                button:hover span {
-                  background-color: #fff;
-                  color: #257830;
-                }
-              `}</style>
-            </button>
-          </div>
         </div>
       </div>
       {/* Additional Footer Content */}
